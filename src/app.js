72072b2a36467998a368d0446296dbef363aea22
Nizam <ramazanov570633@gmail.com>
2023-08-05 23:05:47 +0400

    render api

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }))
 mongoose.connect(CONNECTION_STRING)                                                     
 .then(res=>console.log('connect'))
 .catch(err=>console.log(err))
-app.options("*", cors({ origin: ['http://localhost:5555', "https://marvel-backend2.onrender.com"], optionsSuccessStatus: 200 }));
+app.options("*", cors({ origin: ['http://localhost:5555', "https://chat-backend-ulkc.onrender.com"], optionsSuccessStatus: 200 }));
 app.options("*", cors({ origin: '*', optionsSuccessStatus: 200 }));
 
 app.use((err,res)=>{
