16a3ee26246dd9067ceeb398e8eb7387f18efe01
Nizam <ramazanov570633@gmail.com>
2023-08-06 14:15:02 +0400

    login

---
+++ src/models/usermodel.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    enum: ['image'],
+    required: true
+  },
+});
+
+
+userSchema.methods.comparepassword = async function (password) {
+  if (!password) throw new Error('Password is mission, can not compare!');
+
+  try {
+    const result = await bcrypt.compare(password, this.password);
+    return result;
+  } catch (error) {
+    console.log('Error while comparing password!', error.message);
+  }
+};
+
+userSchema.statics.isThisEmailInUse = async function (email) {
+  if (!email) throw new Error('Invalid Email');
+  try {
+    const user = await this.findOne({ email });
+    if (user) return false;
+
+    return true;
+  } catch (error) {
+    console.log('error inside isThisEmailInUse method', error.message);
+    return false;
+  }
+};
+
+module.exports = mongoose.model('User', userSchema);
